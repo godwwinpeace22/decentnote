dfb3c6408e3416c1e96c5dc43473a581bc252715
Godwin Gabriel <godwinpeace22@gmail.com>
2019-09-23 11:10:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar
+    app
+    clipped-left
+    color="amber"
+  >
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <span class="title ml-3 mr-5">Google&nbsp;<span class="font-weight-light">Keep</span></span>
+    <v-text-field
+      solo-inverted
+      flat
+      hide-details
+      label="Search"
+      prepend-inner-icon="search"
+    ></v-text-field>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
